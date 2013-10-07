0ed93919c05b15c462eba95acbc74e944216c845
nightwing <amirjanyan@gmail.com>
2013-10-07 11:41:34 +0400

    disable all broken tests

--- gui_test.js
+++ gui_test.js
@@ -133,7 +133,7 @@ require(["lib/architect/architect", "lib/chai/chai", "/vfs-root"],
         
         // Mock plugins
         {
-            consumes : ["apf", "ui"],
+            consumes : ["apf", "ui", "Plugin"],
             provides : [
                 "commands", "menus", "layout", "watcher", 
                 "save", "preferences", "clipboard"
