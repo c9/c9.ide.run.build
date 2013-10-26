8d396df7b569f6453e08820a1afbf1edeeaba0a5
Ruben Daniels <ruben@c9.io>
2013-10-26 02:31:41 +0000

    Fixed build gui test

--- gui_test.js
+++ gui_test.js
@@ -135,8 +135,8 @@ require(["lib/architect/architect", "lib/chai/chai", "/vfs-root"],
         {
             consumes : ["apf", "ui", "Plugin"],
             provides : [
-                "commands", "menus", "layout", "watcher", 
-                "save", "preferences", "clipboard"
+                "commands", "menus", "layout", "watcher", "save", 
+                "preferences", "clipboard", "dialog.alert", "auth.bootstrap"
             ],
             setup    : expect.html.mocked
         },
